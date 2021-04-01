c65c1de3376aca5c60b72558151b6ee9feae5304
dennismutia <dennis.mutia@gmail.com>
2021-04-01 08:38:55 +0300

    feat: configure to use ajax fetch method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -11,17 +11,17 @@ class Todo(db.Model):
     description = db.Column(db.String(), nullable=False)    
 
     def __repr__(self):
-        return f'<Todo {self.id} {self.description}'
-
-db.create_all()
+        return f'<Todo {self.id} {self.description}>'
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.form.get('description', '')
+    description = request.get_json()['description']
     todo = Todo(description=description)
     db.session.add(todo)
     db.session.commit()
-    return redirect(url_for('index'))
+    return jsonify({
+        'description': todo.description
+    })
 
 @app.route('/')
 def index():
