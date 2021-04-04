2c0a91149e1e7602b02f5dddd01d04f225f5eebd
dennismutia <dennis.mutia@gmail.com>
2021-04-04 16:09:44 +0300

    fix: minor bug fixes

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import sys
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://udacitystudios@localhost:5432/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:@localhost:5432/todoapp'
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
-
 
 class Todo(db.Model):
   __tablename__ = 'todos'
@@ -18,12 +18,12 @@ class Todo(db.Model):
   def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
 
-@app.route('/todos/create', method=['POST'])
+@app.route('/todos/create', methods=['POST'])
 def create_todo():
   error = False
   body = {}
   try:
-    description = request.form.get_json()['description']
+    description = request.get_json()['description']
     todo = Todo(description=description)
     db.session.add(todo)
     db.session.commit()
