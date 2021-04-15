ec0a54f568e088a64e77d1f146574b668c8086cc
dennismutia <dennis.mutia@gmail.com>
2021-04-15 08:15:16 +0300

    feat: add todolist model and create migration files for it

--- app.py
+++ app.py
@@ -13,10 +13,19 @@ class Todo(db.Model):
   __tablename__ = 'todos'
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
-  completed = db.Column(db.Boolean, nullable=False)
+  completed = db.Column(db.Boolean, nullable=False, default=False)
+  list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
   def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
+
+class ToDoList(db.Model):
+  __tablename__ = 'todolists'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable=False)
+  todos = db.relationship('Todo', backref='list', lazy=True)
+
+
 
 @app.route('/todos/<todo_id>', methods=['DELETE'])
 def delete_todo(todo_id):
@@ -69,6 +78,12 @@ def set_completed_todo(todo_id):
     db.session.close()
   return redirect(url_for('index'))
 
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+  return render_template('index.html', 
+    todos=Todo.query.filter_by(list_id=list_id).order_by('id')
+    .all())
+
 @app.route('/')
 def index():
-  return render_template('index.html', todos=Todo.query.order_by('id').all())
+  return redirect(url_for('get_list_todos', list_id=1))
